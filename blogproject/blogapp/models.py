e83e338fe889e0e20dea82ce7c4092c065be372b
Tehman700 <tehmanfast@gmail.com>
2025-07-23 09:25:22 +0500

    Published on 23-07-25 at 9:11AM for Assignment 01 Lead Sir Shehroz

--- blogproject/blogapp/models.py
+++ blogproject/blogapp/models.py
@@ -4,22 +4,26 @@ from django.conf import settings
 
 
 
+
+# Below is the Model for Users that contain all the necessary fields for Registering the User
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     mobile_number = models.CharField(max_length=15)
     ROLE_CHOICES = (
-        ('writer', 'Writer'),
+        ('writer', 'Writer'),                           # Role choices are used so that we can change permissions accordingly
         ('viewer', 'Viewer'),
     )
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
-    REQUIRED_FIELDS = ['email', 'mobile_number', 'role', 'first_name', 'last_name']
+    REQUIRED_FIELDS = ['email', 'mobile_number', 'role', 'first_name', 'last_name']          # These are the rest fields required
 
 
 
 
 
-# THis is the model for blogpost
+
+
+# THis is the model for blogpost that contains the title, and content which users will write in JSON rest will be automatically fetched
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=255)
