ec0d2e76c9ff347f92e856dee218726558921623
Tehman700 <tehmanfast@gmail.com>
2025-07-28 01:56:42 +0500

    Added the Next Features like Likes and Dislikes and Comments

--- blogproject/blogapp/Views/BlogPostViewSet.py
+++ blogproject/blogapp/Views/BlogPostViewSet.py
@@ -41,7 +41,6 @@ class BlogPostViewSet(viewsets.ModelViewSet):
             }, status=200)
 
 
-
 # Retrieve function is called when we want to get specific id or blogpost with id
     def retrieve(self, request, *args, **kwargs):  # GET /api/blogs/<id>/
         try:
