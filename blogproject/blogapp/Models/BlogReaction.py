043baca93b7a391abea80d08f204d1e1ee4aba4a
Tehman700 <tehmanfast@gmail.com>
2025-07-27 23:01:56 +0500

    Added the Next Features like Likes and Dislikes and Comments

--- blogproject/blogapp/Models/BlogReaction.py
+++ blogproject/blogapp/Models/BlogReaction.py
@@ -7,7 +7,6 @@ class BlogReaction(models.Model):
     REACTION_CHOICES = (
     ('like', 'Like'), ('dislike', 'Dislike'),
     )
-
     reaction_type = models.CharField(max_length=10, choices=REACTION_CHOICES)
     class Meta:
         unique_together = ('user', 'blog_post')
