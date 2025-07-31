cf3734d6e89042fc331938a98d049baa7645caf4
Tehman700 <tehmanfast@gmail.com>
2025-07-31 21:43:38 +0500

    Created all the tests and made the code more clean

--- blogproject/blogapp/Serializers/RegisterSerializer.py
+++ blogproject/blogapp/Serializers/RegisterSerializer.py
@@ -1,12 +1,6 @@
 from rest_framework import serializers
 from blogapp.models import User
 
-"""
-For the Register Serializer, the Password field is for the authentication and since we are using User model so we defined
-this in the Meta class and other fields which are necessary for Registering,
-The def create function will be only called when the data is validated and we will create a new user specially with this data
-
-"""
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
